5454da8511981598c6e5a0b6e1a51febac0cbbaa
yingli1990315 <yingli1990315@gmail.com>
2017-05-01 19:04:11 -0700

    first commit

---
+++ src/main/java/com/adminportal/service/impl/BookServiceImpl.java
@@ -0,0 +1,42 @@
+package com.adminportal.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adminportal.domain.Book;
+import com.adminportal.repository.BookRepository;
+import com.adminportal.service.BookService;
+
+@Service
+public class BookServiceImpl implements BookService{
+
+	@Autowired
+	private BookRepository bookRepository;
+	
+	@Override
+	public Book save(Book book){
+		return bookRepository.save(book);
+	}
+
+
+	@Override
+	public List<Book> findAll() {
+		return (List<Book>)bookRepository.findAll();
+	}
+
+
+	@Override
+	public Book findOne(Long id) {
+		return bookRepository.findOne(id);
+	}
+
+
+	@Override
+	public void removeOne(Long id) {
+		bookRepository.delete(id);
+	}
+	
+	
+}
